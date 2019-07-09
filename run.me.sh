90d20eecce2d4349a4df60b973740508053aed63
Ladar Levison <ladar@lavabit.com>
2019-07-09 04:58:23 -0500

    Added autoreconf to run me script.

--- run.me.sh
+++ run.me.sh
@@ -1,3 +1,5 @@
-# git clone https://github.com/ckamm/sedutil && cd sedutil && autoreconf --install
+# git clone https://github.com/ckamm/sedutil && cd sedutil
+if [ ! -f ./configure ]; then autoreconf --install ; fi
 ./configure && make all && cd images && ./getresources && ./buildpbaroot && ./buildbios && ./buildUEFI64 && mkdir UEFI64 && mv UEFI64-1.15*.img.gz UEFI64 && ./buildrescue Rescue32 && ./buildrescue Rescue64 && cd ..
+printf "\n\nAll done.\nMaking noise so you know we're done.\n\n\n"
 while true; do printf "\a"; sleep 2; done
