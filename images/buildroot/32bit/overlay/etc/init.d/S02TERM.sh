254f2a58c96d0b1c5f34e0b38bd054ec10b91504
Ladar Levison <ladar@lavabit.com>
2020-10-24 06:52:37 +0000

    Added the KBD package.

--- images/buildroot/32bit/overlay/etc/init.d/S02TERM.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-clear
-[ -f /sys/devices/system/cpu/intel_pstate/no_turbo ] && (echo '1' > /sys/devices/system/cpu/intel_pstate/no_turbo)
-/usr/sbin/setfont /usr/share/consolefonts/Uni1-VGA32x16.psf
